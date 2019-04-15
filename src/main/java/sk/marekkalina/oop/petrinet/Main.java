1c804dd80da8670dc6d036289b9047dc99617724
marek <markus4677@gmail.com>
2019-04-15 04:37:36 +0200

    Test

--- src/main/java/sk/marekkalina/oop/petrinet/Main.java
+++ src/main/java/sk/marekkalina/oop/petrinet/Main.java
@@ -1,5 +1,10 @@
 package sk.marekkalina.oop.petrinet;
 
-public class Main {
-  System.Out.Println("Hello");
+public class Main{
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
 }
